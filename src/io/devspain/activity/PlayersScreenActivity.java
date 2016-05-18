9ab4b38fba9fdf96a554db51a647f5399c5346af
Piercing <merlosalbarracin@gmail.com>
2016-05-18 05:09:38 +0200

    Change the interface to activity PlayersSCreenActivity

--- src/io/devspain/activity/PlayersScreenActivity.java
+++ src/io/devspain/activity/PlayersScreenActivity.java
@@ -2,15 +2,18 @@ package io.devspain.activity;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import io.devspain.R;
 import io.devspain.fragments.EngagePlayersFragment;
+import io.devspain.fragments.EngagePlayersFragment.OnPlayerSelectedListener;
 import io.devspain.fragments.RetirePlayersFragment;
 
-public class PlayersScreenActivity extends FragmentActivity implements ActionBar.TabListener {
+public class PlayersScreenActivity extends FragmentActivity implements ActionBar.TabListener, OnPlayerSelectedListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,6 +26,16 @@ public class PlayersScreenActivity extends FragmentActivity implements ActionBar
 		// Add the tabs
 		ab.addTab(ab.newTab().setText("Para Jubilar").setTabListener(this));
 		ab.addTab(ab.newTab().setText("para Fichar").setTabListener(this));
+
+		// Charge the layout of fragmente
+		setContentView(R.layout.activity_edition_player);
+
+		FragmentManager FM = getFragmentManager();
+		FragmentTransaction FT = FM.beginTransaction();
+
+		Fragment fragmentEngage = new Fragment();
+		FT.replace(R.id.container, fragmentEngage);
+		FT.commit();
 	}
 
 	@Override
@@ -55,6 +68,23 @@ public class PlayersScreenActivity extends FragmentActivity implements ActionBar
 	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void onPlayerSelected(String str) {
+		Fragment fragmentEngageSelected = new Fragment();
+
+		Bundle args = new Bundle();
+		args.putString("str", str);
+		fragmentEngageSelected.setArguments(args);
+
+		FragmentManager FM = getFragmentManager();
+		FragmentTransaction FT = FM.beginTransaction();
+
+		FT.replace(R.id.container, fragmentEngageSelected);
+		FT.addToBackStack(null);
+
+		FT.commit();
 	}
 
 }
